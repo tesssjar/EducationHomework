5afdb5e745f982b4c3499b2a152cca80e212015e
Ivan <tesjarr@gmail,com>
2022-01-15 22:13:18 +0200

    Разбил программу и классы на отдельные файлы

---
+++ ClassEmployer/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassStudent;
+
+
+namespace ClassEmployer
+{
+    public class Employer
+    {
+        public string empName;
+
+        public Employer(string Name)
+        {
+            empName = Name;
+        }
+
+        public string Name
+        {
+            get { return empName; }
+            set => empName = value;
+        }
+
+        public List<Student> Employee { get; set; }
+
+        public static void AddStudent(Student student, List<Student> employee) => employee.Add(student);
+
+        public static double SalaryCount(Student student)
+        {
+            double res = (Student.IndexOfHappiness(student.KdmMark, student.VmMark) / 100.0) * 20000.0;
+
+            return res;
+        }
+    }
+}
